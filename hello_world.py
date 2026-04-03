0270f31607dfe04d52f2ff88995fa4ca8442a6e3
547c <whdudfo20060413@gmail.com>
2026-04-03 01:09:06 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+def greeting():
+
+    name = "547c"
+
+    goal = "ASU or UT Dallas"
+
+    print(f"Hello, I'm {name} from ETAMU!")
+
+    print(f"I'm studying Python and Math to transfer to {goal}.")if __name__ == "__main__":
+
+    greeting()
